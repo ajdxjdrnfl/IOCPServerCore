51cadac8e1ebc87a7f42b58d03425b72fdeb9b3e
ajdxjdrnfl <ajdxjdrnfl>
2024-03-11 16:26:04 +0900

    add : memory pool

--- ServerEngine/MemoryPool.cpp
+++ ServerEngine/MemoryPool.cpp
@@ -1,2 +1,37 @@
 #include "pch.h"
 #include "MemoryPool.h"
+
+
+MemoryPool::MemoryPool(int32 allocSize) : _allocSize(allocSize)
+{
+
+}
+
+MemoryPool::~MemoryPool()
+{
+	while (!_queue.empty())
+	{
+		 auto& k = _queue.front();
+		 delete k;
+		 k = nullptr;
+	}
+}
+
+
+MemoryHeader* MemoryPool::Pop()
+{
+	if (!_queue.empty())
+	{
+		MemoryHeader* ptr =	_queue.front();
+		_queue.pop();
+		return ptr;
+	}
+
+	malloc(_allocSize);
+
+}
+
+void MemoryPool::Push()
+{
+
+}
--- ServerEngine/MemoryPool.h
+++ ServerEngine/MemoryPool.h
@@ -1,5 +1,22 @@
 #pragma once
+
+struct MemoryHeader
+{
+	int32 allocSize;
+};
+
 class MemoryPool
 {
+public:
+	MemoryPool(int32 allocSize);
+	~MemoryPool();
+
+	MemoryHeader* Pop();
+	void Push();
+
+private:
+	USE_LOCK(POOL)
+	queue<MemoryHeader*> _queue;
+	int32 _allocSize;
 };
 
